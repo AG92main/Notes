20e32ac1052b8946fe3722ca3f78320b98195c08
Андрей Говорухин <ag92main@gmail.com>
2022-04-02 18:05:15 +0300

    titeleView

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     private var rightBarButton = UIBarButtonItem()
-    private var placehlderTextView = UITextField()
-    private var titleView = UILabel()
+    private var titleVeiw = UITextField()
     private var mainTextView = UITextView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupRightBarButton()
-        setupplacehlderTextView()
-        setupTitleTextView()
         setupMainTextView()
         mainTextView.becomeFirstResponder()
     }
@@ -26,18 +23,30 @@ class ViewController: UIViewController {
         rightBarButton.target = self
         navigationItem.rightBarButtonItem = rightBarButton
     }
-    private func setupplacehlderTextView() {
-        view.addSubview(placehlderTextView)
-        placehlderTextView.text = ""
+    private func titleView() {
+        view.addSubview(titleVeiw)
+        titleVeiw.translatesAutoresizingMaskIntoConstraints = false
+        titleVeiw.text = "Заметка"
+        titleVeiw.font = .boldSystemFont(ofSize: 22)
+        titleVeiw.trailingAnchor.constraint(
+            lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -20
+        ).isActive = true
     }
-    private func setupTitleTextView() {
-        view.addSubview(titleView)
-        titleView.text = "Заметка"
-        titleView.font = .boldSystemFont(ofSize: 22)
-    }
+
     private func setupMainTextView() {
         view.addSubview(mainTextView)
+        mainTextView.translatesAutoresizingMaskIntoConstraints = false
         mainTextView.text = "Текст Заметки"
         mainTextView.font = .systemFont(ofSize: 14)
+        mainTextView.leadingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+            constant: 20
+        ).isActive = true
+        mainTextView.trailingAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+            constant: -20
+        ).isActive = true
+        mainTextView.heightAnchor.constraint(equalToConstant: 80).isActive = true
     }
 }
