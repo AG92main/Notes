247e1acf59fcc99308bdbc8b9750ac26a3eba155
Андрей Говорухин <ag92main@gmail.com>
2022-04-12 00:20:27 +0300

    NoteModel

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Notes
+//
+//  Created by Andrey on 11.04.2022.
+//
+
+import Foundation
---
+++ Notes/NoteModel.swift
@@ -0,0 +1,13 @@
+//
+//  SomeModel.swift
+//  Notes
+//
+//  Created by Andrey on 11.04.2022.
+//
+
+import Foundation
+
+struct SomeModel {
+    var title: String?
+    var text: String?
+}
